5bcd21f6abbb7a6c74a459b4f4318fda9ed425bd
--replace-all <truongnbn@bkav.com>
2021-05-23 23:54:34 +0700

    two

---
+++ WebStudent/Controllers/SinhVienController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStudent.Models;
+using WebStudent.Repository;
+
+namespace WebStudent.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        public ActionResult GetAllSVDetails()
+        {
+            SinhVienRepository SVRepo = new SinhVienRepository();
+            ModelState.Clear();
+            return View(SVRepo.GetAllSV());
+        }
+        // GET: NhanVien
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: NhanVien/Details/5
+        public ActionResult Details(int maSV)
+        {
+            SinhVienRepository SVRepo = new SinhVienRepository();
+            return View(SVRepo.GetAllSV().Find(sv => sv.MaSV == maSV));
+        }
+
+        // POST: NhanVien/Details/5
+        [HttpPost]
+
+        public ActionResult EditDetails(int id, SinhVienModel obj)
+        {
+            try
+            {
+                SinhVienRepository svRepo = new SinhVienRepository();
+
+                svRepo.UpdateSV(obj);
+                return RedirectToAction("GetAllEmpDetails");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhanVien/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: NhanVien/Create
+        [HttpPost]
+        public ActionResult Create(SinhVienModel sv)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    SinhVienRepository SvRepo = new SinhVienRepository();
+
+                    if (SvRepo.AddSV(sv))
+                    {
+                        ViewBag.Message = "Employee details added successfully";
+                    }
+                }
+
+                return View();
+            }
+            catch(Exception ex)
+            {
+                return View();
+            }
+        }
+
+        // GET: NhanVien/Edit/5
+        public ActionResult Edit(int maSV)
+        {
+            SinhVienRepository SVRepo = new SinhVienRepository();
+            return View(SVRepo.GetAllSV().Find(sv => sv.MaSV == maSV));
+        }
+
+        // POST: NhanVien/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, SinhVienModel obj)
+        {
+            try
+            {
+                SinhVienRepository svRepo = new SinhVienRepository();
+
+                svRepo.UpdateSV(obj);
+                return RedirectToAction("GetAllEmpDetails");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhanVien/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                SinhVienRepository svRepo = new SinhVienRepository();
+                if (svRepo.DeleteSV(id))
+                {
+                    ViewBag.AlertMsg = "Employee details deleted successfully";
+
+                }
+                return RedirectToAction("GetAllEmpDetails");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: NhanVien/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebStudent/Repository/SinhVienRepository.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebStudent.Models;
+
+namespace WebStudent.Repository
+{
+    public class SinhVienRepository
+    {
+        private SqlConnection con;
+        //To Handle connection related activities    
+        private void connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["getconn"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+        //To Add Employee details    
+        public bool AddSV(SinhVienModel obj)
+        {
+
+            connection();
+            SqlCommand com = new SqlCommand("AddSinhVien", con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@HoTen", obj.HoTen);
+            com.Parameters.AddWithValue("@NgaySinh", obj.NgaySinh);
+            com.Parameters.AddWithValue("@GioiTinh", obj.GioiTinh);
+            com.Parameters.AddWithValue("@DiaChi", obj.DiaChi);
+            com.Parameters.AddWithValue("@MaLop", obj.MaLop);
+
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+            }
+
+
+        }
+        //To view employee details with generic list     
+        public List<SinhVienModel> GetAllSV()
+        {
+            connection();
+            List<SinhVienModel> EmpList = new List<SinhVienModel>();
+
+
+            SqlCommand com = new SqlCommand("GetSinhVien", con);
+            com.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter da = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            da.Fill(dt);
+            con.Close();
+            //Bind EmpModel generic list using dataRow     
+            foreach (DataRow dr in dt.Rows)
+            {
+
+                EmpList.Add(
+
+                    new SinhVienModel
+                    {
+
+                        MaSV = Convert.ToInt32(dr["MaSV"]),
+                        HoTen = Convert.ToString(dr["HoTen"]),
+                        NgaySinh = Convert.ToString(dr["NgaySinh"]),
+                        GioiTinh = Convert.ToString(dr["GioiTinh"]),
+                        DiaChi = Convert.ToString(dr["DiaChi"]),
+                        MaLop = Convert.ToString(dr["MaLop"])
+                    }
+                    );
+            }
+
+            return EmpList;
+        }
+        //To Update Employee details    
+        public bool UpdateSV(SinhVienModel obj)
+        {
+
+            connection();
+            SqlCommand com = new SqlCommand("UpdateSinhVien", con);
+
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@MaSV", obj.MaSV);
+            com.Parameters.AddWithValue("@HoTen", obj.HoTen);
+            com.Parameters.AddWithValue("@NgaySinh", obj.NgaySinh);
+            com.Parameters.AddWithValue("@GioiTinh", obj.GioiTinh);
+            com.Parameters.AddWithValue("@DiaChi", obj.DiaChi);
+            com.Parameters.AddWithValue("@MaLop", obj.MaLop);
+
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        //To delete Employee details    
+        public bool DeleteSV(int MaSV)
+        {
+
+            connection();
+            SqlCommand com = new SqlCommand("DeleteSinhVien", con);
+
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@MaSV", MaSV);
+
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+    }
+}
