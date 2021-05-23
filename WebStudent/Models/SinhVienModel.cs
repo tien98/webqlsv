36d1f3f2d169b0cc6093a819ef08cd4b467a1c1f
--replace-all <truongnbn@bkav.com>
2021-05-23 21:21:50 +0700

    init

---
+++ WebStudent/Models/SinhVienModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebStudent.Models
+{
+    /// <summary>
+    /// Entity Sinh Vien
+    /// </summary>
+    public class SinhVienModel
+    {
+        public int MaSV { get; set; }
+        public string HoTen { get;set;}
+        public string NgaySinh { get; set; }
+        public string GioiTinh { get; set; }
+        public string DiaChi { get; set; }
+        public string MaLop { get; set; }
+    }
+}
